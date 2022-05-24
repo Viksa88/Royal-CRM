5682149a54a3fa2c09c3382d7c97fc8c1f49a680
viksa88 <vic.bud22@gmail.com>
2022-05-24 10:34:58 +0300

    orders+products+customers

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,45 +1,62 @@
 const database = require('./database');
+const joi = require('joi');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
-    addOrder: async function (order_time, price, quantity) {
-        const sql = "INSERT INTO orders(order_time, price, quantity)" +
-            " VALUES(?,?,?);";
-    },
+    //  addOrder: function() {
+    //     const name = process.argv.slice(2);
+    
+    //     if (!name || name.length === 0) {
+    //         throw ('ERROR: name is empty');
+    //     }
+    
+    //     this.orders.push({
+    //         name: name,
+    //         id: this.orders.length,
+    //     });        
+    // },
 
-    ordersList: async function (req, res, next) {
-        const sql = "SELECT * FROM orders";
+    ordersList: async function(req, res, next){
+        const sql = "SELECT orders.id, orders.order_time, orders.price, orders.quantity, "+ 
+        "orders.product_name, orders.product_desc, orders.product_image, cust.id AS customer_id, "+
+        "cust.name, cust.phone, cust.email FROM orders orders LEFT JOIN customers cust "+
+        "ON orders.id = cust.id ORDER BY orders.id ASC;";
 
-        try {
+        try {    
             const result = await database.query(sql);
             res.send(result[0]);
-        } catch (err) {
+        } 
+        catch (err) {
             console.log(err);
         }
     },
 
-        
-    exportOrders: async function () {
-        const sql = "SELECT order_time, price, quantity FROM orders ORDER BY name ASC;";
+    exportOrders: async function (req, res, next) {
+        const sql = "SELECT orders.order_time, orders.price, orders.quantity, "+ 
+        "orders.product_name, orders.product_desc, orders.product_image, "+
+        "cust.name, cust.phone, cust.email FROM orders orders LEFT JOIN customers cust "+
+        "ON orders.id = cust.id ORDER BY orders.id ASC;";
+
+        try {
+            const result = await database.query(sql);
+
+            const now = new Date().getTime(); // moment.js
+            const filePath = path.join(__dirname, '../files', `orders-${now}.txt`);
+            const stream = fs.createWriteStream(filePath);
+
+            stream.on('open', function () {
+                stream.write(JSON.stringify(result[0]));
+                stream.end();
+            });
+
+            stream.on('finish', function () {
+                res.send(`Success. File at: ${filePath}`);
+            });
+        }
+        catch (err) {
+            throw err;
+        }
+
     },
-
-    
-    editOrders: async function () {
-       
-
-     },
-
-    
-    deleteOrders: async function () {
-        
-     },
-
-    
-    searchOrders: async function () {
-      
-     },
-
-     addOrders: async function () {
-
-     }
-
 }
--- controllers/products.js
+++ controllers/products.js
@@ -1,35 +1,50 @@
 const database = require('./database');
+const joi = require('joi');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
-    // todo: update to use resent changes...
+    addProduct: async function (req, res, next) {
+        const reqBody = req.body;
 
-    addProduct: async function (name, desc, price) {
-        //     if (!name || name.length === 0) {
-        //         throw ('ERROR: name is empty');
-        //     }
+        const schema = joi.object({
+            name: joi.string().required().min(2).max(100),
+            description: joi.string().required().min(2).max(300),
+            price: joi.number().required(),
+        });
 
-        //     database.pool.getConnection(function (connErr, connection) {
-        //         if (connErr) throw connErr; // not connected!
+        const { error, value } = schema.validate(reqBody);
+
+        if (error) {
+            res.send(`error adding product: ${error}`);
+            return;
+        }
 
         const sql = "INSERT INTO products(name, description, price)" +
             " VALUES(?,?,?);";
 
-        //         connection.query(
-        //             sql,
-        //             [name, desc, price],
-        //             function (sqlErr, result, fields) {
-        //                 if (sqlErr) throw sqlErr;
+        try {
+            const result = await database.query(
+                sql,
+                [
+                    reqBody.name,
+                    reqBody.description,
+                    reqBody.price,
+                ]
+            );
+        }
+        catch (err) {
+            console.log(err);
+            return;
+        }
 
-        //                 console.log(result);
-        //             });
-        //     });
+        res.send(`${reqBody.name} added successfully`);
     },
 
     productsList: async function (req, res, next) {
         const sql = "SELECT * FROM products ORDER BY name ASC;";
 
         try {
-            // const connection = await database.getConnection();
             const result = await database.query(sql);
             res.send(result[0]);
         }
@@ -39,25 +54,47 @@ module.exports = {
     },
 
     // todo: search product by name
-    exportProducts: async function () {
+    exportProducts: async function (req, res, next) {
         const sql = "SELECT name,description,price FROM products ORDER BY name ASC;";
+
+        try {
+            const result = await database.query(sql);
+
+            const now = new Date().getTime(); // moment.js
+            const filePath = path.join(__dirname, '../files', `products-${now}.txt`);
+            const stream = fs.createWriteStream(filePath);
+
+            stream.on('open', function () {
+                stream.write(JSON.stringify(result[0]));
+                stream.end();
+            });
+
+            stream.on('finish', function () {
+                res.send(`Success. File at: ${filePath}`);
+            });
+        }
+        catch (err) {
+            throw err;
+        }
     },
 
     // todo: edit product details
-    editProduct: async function () {
+    editProduct: async function (req, res, next) {
         // const sql = UPDATE
-
-     },
+        res.send('todo update products');
+    },
 
     // todo: delete product
-    deleteProduct: async function () {
+    deleteProduct: async function (req, res, next) {
         // const sql = DROP
-     },
+        res.send('todo delete products');
+    },
 
     // todo: search product by name
-    searchProducts: async function () {
+    searchProducts: async function (req, res, next) {
         // const sql = SELECT WHERE...
-     },
+        res.send('todo search products');
+    },
 
     // todo: sort products by name...
 }
