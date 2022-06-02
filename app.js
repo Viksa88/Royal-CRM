32d52a8c418e972c9fd14d7ebc339bac129c3d97
viksa88 <vic.bud22@gmail.com>
2022-06-02 19:37:45 +0300

    fdgdfg

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const createError = require('http-errors');
+
+const auth = require('./middleware/auth');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,9 +21,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/customers', customersRouter);
-app.use('/products', productsRouter);
-app.use('/orders', ordersRouter);
+app.use('/users', auth, usersRouter);
+app.use('/customers', auth, customersRouter);
+app.use('/products', auth, productsRouter);
+app.use('/orders', auth, ordersRouter);
+
+// catch 404 err forward error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+})
+
+// custom error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.locals.message = err.message;
+    res.locals.error = err;
+
+    res.status(500).send(err);
+})
 
 module.exports = app;
